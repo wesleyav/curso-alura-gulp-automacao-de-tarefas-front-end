64d0155d3e3527149278a21d8f60408ec574a96f
Wesley <wesley.contato@hotmail.com>
2020-08-18 07:25:31 -0300

    Criando tasks

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -1,13 +1,22 @@
 var gulp = require('gulp'),
-    imagemin = require('gulp-imagemin');
+    imagemin = require('gulp-imagemin'),
+    clean = require('gulp-clean');
 
-gulp.task('build-img', function () {
+gulp.task('copy', ['clean'], function () {
 
-    gulp.src('src/img/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('src/img'));
+    return gulp.src('src/**/*')
+        .pipe(gulp.dest('dist'));
 });
 
+gulp.task('clean', function () {
 
+    return gulp.src('dist')
+        .pipe(clean());
+});
 
+gulp.task('build-img', ['copy'], function () {
 
+    gulp.src('dist/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+});
