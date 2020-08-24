aa66388081ecfb435887241d0fc0ebd9a48af9f4
Wesley <wesley.contato@hotmail.com>
2020-08-24 05:52:08 -0300

    Criando tarefas e definindo tarefa padrão

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -1,6 +1,12 @@
-var gulp = require('gulp'),
-    imagemin = require('gulp-imagemin'),
-    clean = require('gulp-clean');
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var htmlReplace = require('gulp-html-replace');
+
+gulp.task('default', ['copy'], function () {
+    gulp.start('build-img', 'build-js', 'build-html');
+});
 
 gulp.task('copy', ['clean'], function () {
 
@@ -14,9 +20,27 @@ gulp.task('clean', function () {
         .pipe(clean());
 });
 
-gulp.task('build-img', ['copy'], function () {
+gulp.task('build-img', function () {
 
     gulp.src('dist/img/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('build-js', function () {
+
+    gulp.src(['dist/js/jquery.js', 'dist/js/home.js', 'dist/js/produto.js'])
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist/js'));
+
+});
+
+gulp.task('build-html', function () {
+
+    gulp.src('dist/**/*.html')
+        .pipe(htmlReplace({
+            js: 'js/all.js'
+        }))
+        .pipe(gulp.dest('dist'));
+
 });
